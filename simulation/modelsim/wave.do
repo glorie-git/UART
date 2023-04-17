6ea76dfccc2b3dcc4f8483f44abec549a40de3c9
Glorie Ramazani <glorieramazani@gmail.com>
2023-04-17 13:06:48 -0700

    fixes toplevel tb *all tests passed*add more tests

--- simulation/modelsim/wave.do
+++ simulation/modelsim/wave.do
@@ -1,9 +1,67 @@
-
-restart -f
-
-force -freeze sim:/uart/transmitter 1 1000
-force -freeze sim:/uart/FAST_CLOCK 1 0, 0 {50 ps} -r 100
-force -freeze sim:/uart/tx_data 00110110 0
-force -freeze sim:/uart/UART_CTS 1 0
-
-run
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate -divider Testbench
+add wave -noupdate /tb_uart/intii
+add wave -noupdate -divider {Device A}
+add wave -noupdate /tb_uart/deviceA/FAST_CLOCK
+add wave -noupdate /tb_uart/deviceA/transmitter
+add wave -noupdate /tb_uart/deviceA/tx_data
+add wave -noupdate /tb_uart/deviceA/rx_data
+add wave -noupdate /tb_uart/deviceA/UART_TXD
+add wave -noupdate /tb_uart/deviceA/UART_RXD
+add wave -noupdate /tb_uart/deviceA/UART_CTS
+add wave -noupdate /tb_uart/deviceA/UART_RTS
+add wave -noupdate /tb_uart/deviceA/packet
+add wave -noupdate /tb_uart/deviceA/rx_packet
+add wave -noupdate /tb_uart/deviceA/parityBit
+add wave -noupdate /tb_uart/deviceA/baudClock
+add wave -noupdate /tb_uart/deviceA/baudRateCounter
+add wave -noupdate /tb_uart/deviceA/number
+add wave -noupdate /tb_uart/deviceA/state
+add wave -noupdate /tb_uart/deviceA/sync_err
+add wave -noupdate /tb_uart/deviceA/pkt_create_done
+add wave -noupdate /tb_uart/deviceA/pkt_process_done
+add wave -noupdate /tb_uart/deviceA/parity_err
+add wave -noupdate /tb_uart/deviceA/parity_err_check_done
+add wave -noupdate /tb_uart/deviceA/txDone
+add wave -noupdate /tb_uart/deviceA/rxDone
+add wave -noupdate -divider {Divice B}
+add wave -noupdate /tb_uart/device_B/transmitter
+add wave -noupdate /tb_uart/device_B/tx_data
+add wave -noupdate /tb_uart/device_B/rx_data
+add wave -noupdate /tb_uart/device_B/UART_TXD
+add wave -noupdate /tb_uart/device_B/UART_RXD
+add wave -noupdate /tb_uart/device_B/UART_CTS
+add wave -noupdate /tb_uart/device_B/UART_RTS
+add wave -noupdate /tb_uart/device_B/data
+add wave -noupdate /tb_uart/device_B/packet
+add wave -noupdate /tb_uart/device_B/rx_packet
+add wave -noupdate /tb_uart/device_B/parityBit
+add wave -noupdate /tb_uart/device_B/baudClock
+add wave -noupdate /tb_uart/device_B/baudRateCounter
+add wave -noupdate /tb_uart/device_B/state
+add wave -noupdate /tb_uart/device_B/sync_err
+add wave -noupdate /tb_uart/device_B/txDone
+add wave -noupdate /tb_uart/device_B/pkt_create_done
+add wave -noupdate /tb_uart/device_B/pkt_process_done
+add wave -noupdate /tb_uart/device_B/parity_err
+add wave -noupdate /tb_uart/device_B/parity_err_check_done
+add wave -noupdate /tb_uart/device_B/rxDone
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 1} {2443900000 ps} 0}
+quietly wave cursor active 1
+configure wave -namecolwidth 230
+configure wave -valuecolwidth 100
+configure wave -justifyvalue left
+configure wave -signalnamewidth 0
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ps
+update
+WaveRestoreZoom {0 ps} {7633633280 ps}
--- tb_uart.vhd
+++ tb_uart.vhd
@@ -1,4 +1,4 @@
--- final project: uart- testbench
+-- final project: uart - testbench
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -14,6 +14,7 @@ architecture tb of tb_uart is
    TYPE test_case_record IS RECORD
        tx_data : std_logic_vector(7 downto 0);
        rx_data : std_logic_vector(7 downto 0);
+       packet : std_logic_vector(10 downto 0);
    END RECORD;
 
 --    Define a type that is an array of the record.
@@ -27,40 +28,35 @@ architecture tb of tb_uart is
 --    represent inputs to apply, and one represents the expected output.
     
    signal test_case_array : test_case_array_type := (
-        ("11010000","11010000"),
-        ("11010011", "11010011"),
-        ("11000110","11000110"),
-        ("00011100","00011100"),
-        ("10010110","10010110") 
+        ("11010000","11010000","01101000011"),
+        ("11010011", "11010011","01101001111"),
+        ("11000110","11000110","01100011001"),
+        ("00011100","00011100","00001110011"),
+        ("10010110","10010110","01001011001") 
         );   
 
--- component uart is
---      generic(
--- 			CLOCK_INT : integer := 50000000
---         );
--- 	 port(
--- 			FAST_CLOCK : in  std_logic;  -- Clock pin
---             transmitter : in std_logic; -- control signal, if 1 UART is transmitter
---             tx_data : in std_logic_vector(7 downto 0); -- tx input buffer
--- 			rx_data : out std_logic_vector(7 downto 0); -- rx output buffer
--- 			UART_TXD : out std_logic;
--- 			UART_RXD : in  std_logic;-- := '0';
--- 			UART_CTS : in  std_logic; -- := '0';
--- 			UART_RTS : out std_logic -- := '0'
--- 		 );
--- end component uart;
-
-signal FAST_CLOCK :  std_logic := '1';  -- Clock pin
+signal FAST_CLOCK :  std_logic := '0';  -- Clock pin
 signal tx_data, tx_data_B : std_logic_vector(7 downto 0);
 signal transmitter, transmitter_B : std_logic; -- control signal, if 1 UART is transmitter
 signal rx_data, rx_data_B : std_logic_vector(7 downto 0);
 signal UART_TXD, UART_TXD_B : std_logic;
-signal UART_RXD, UART_RXD_B :  std_logic; -- := '0';
+signal UART_RXD, UART_RXD_B :  std_logic := '1';
 signal UART_CTS, UART_CTS_B :  std_logic; -- := '0';
 signal UART_RTS, UART_RTS_B: std_logic;
--- signal clock : std_logic := '0';
+signal rx_packet, test: std_logic_vector(10 downto 0);
+signal int, intii : integer ;
 
-constant transmit_wait : integer := 50000000/9600;
+-- We want to interface to 9600 baud UART
+-- 50000000 / 115200 = 434
+constant clks_per_bit : integer := 434;
+
+-- Test Bench uses a 50 Mhz Clock
+constant clk_period : time := 20 ns;
+
+-- 1/115200
+constant bit_period : time := 8680 ns;
+
+signal clock : std_logic;
 
 begin
     -- dut : uart port map (CLOCK_50, UART_TXD, UART_RXD, UART_CTS, UART_RTS);
@@ -76,164 +72,61 @@ begin
             UART_CTS => UART_CTS, 
             UART_RTS => UART_RTS);
 
-    deviceB : entity work.uart(rtl) 
+    device_B : entity work.uart(rtl) 
     generic map(CLOCK_INT => 50000000)
     port map (
-            FAST_CLOCK => FAST_CLOCK,
-            transmitter => transmitter_B,
-            tx_data => tx_data_B,
-            rx_data => rx_data_B,
-            UART_RXD => UART_RXD_B,
-            UART_CTS => UART_CTS_B,
+            FAST_CLOCK => FAST_CLOCK, 
+            transmitter => transmitter_B, 
+            tx_data => tx_data_B, 
+            rx_data => rx_data_B, 
+            UART_TXD => UART_TXD_B,
+            UART_RXD => UART_RXD_B, 
+            UART_CTS => UART_CTS_B, 
             UART_RTS => UART_RTS_B);
 
-    clock_gen: process
-    begin
-        FAST_CLOCK <= not FAST_CLOCK;
-        wait for 50 ps;
-    end process;
-
-    -- connection of device A and B
-    -- UART_RTS_B <= '0';
-    -- UART_RTS <= '0';
-
+   
+    transmitter <= '1';
+    transmitter_B <= '0'; 
+    
     UART_CTS <= UART_RTS_B;
     UART_CTS_B <= UART_RTS;
 
     UART_RXD <= UART_TXD_B;
     UART_RXD_B <= UART_TXD;
+    -- packet <= "01101000011";
 
-
+    FAST_CLOCK <= not FAST_CLOCK after clk_period/2;
 
     process
     begin
 
-        transmitter_B <= '1';
-        transmitter <= '0';
-
-        -- wait for 50 ps;
-
-        tx_data_B <= "11010000";
-        -- wait for 100 ps;
-        -- UART_RTS_B <= '1';
-        
-        -- UART_CTS <= '1';
-        -- wait for 50 ps;
-        -- UART_CTS <= '0';
-
-        wait for transmit_wait*768 ps;
-        
-        -- UART_RTS_B <= '0';
-        -- UART_RTS <= '0';
-        -- UART_RTS_B <= '1';
-        tx_data_B <= "11010011";
-
-        wait for transmit_wait*768 ps;
-         
-        -- wait for 100 ps;
-        -- -- UART_CTS <= '0';
-        -- wait for 100 ps;
-        -- -- UART_CTS <= '1';
-        -- wait for 50 ps;
-        -- -- UART_CTS <= '0';
-
-        -- wait for transmit_wait*12 ps;
-        -- UART_RTS_B <= '0';
-        
-        -- -- DATA <= "11000110";
-        -- -- UART_CTS <= '0';
-        -- wait for 100 ps;
-        -- -- UART_CTS <= '1';
-        -- wait for 50 ps;
-        -- -- UART_CTS <= '0';
-        tx_data_B <= "11000110";
-
-        wait for transmit_wait*768 ps;
-
-        -- wait for transmit_wait*12 ps;
-        -- UART_RTS_B <= '0';
-        
-        -- -- DATA <= "00011100";
-        -- -- UART_CTS <= '0';
-        -- wait for 100 ps;
-        -- -- UART_CTS <= '1';
-        -- wait for 50 ps;
-        -- -- UART_CTS <= '0';
-
-        tx_data_B <= "00011100";
-
-        wait for transmit_wait*768 ps;
-
-        -- wait for transmit_wait*12 ps;
-        -- UART_RTS_B <= '0';
-        
-        -- -- DATA <= "10010110";
-        -- -- UART_CTS <= '0';
-        -- wait for 100 ps;
-        -- -- UART_CTS <= '1';
-        -- wait for 50 ps;
-        -- -- UART_CTS <= '0';
-
-        tx_data_B <= "10010110";
-
-        -- wait for transmit_wait*768 ps;
-
-        -- wait for transmit_wait*12 ps;
-        -- UART_RTS_B <= '0';
-        
-        wait;
-        
-    end process;
-
-    -- process
-    -- begin
-
-    --     -- UART_RTS <= '1';
-    --     -- UART_RTS_B <= '0';
-    --     transmitter_B <= '1';
-    --     transmitter <= '0';
-
-    --     for i in test_case_array'low to test_case_array'high loop
+        for ii in test_case_array'low to test_case_array'high loop
             
-    --         -- Print information about the testcase to the transcript window (make sure when
-    --         -- you run this, your transcript window is large enough to see what is happening)
-            
-    --         report "-------------------------------------------";
-    --         report "Test case " & integer'image(i) & ":" &
-    --                 " tx_data=" & integer'image(to_integer(unsigned(test_case_array(i).tx_data)));
+            intii <= ii;
 
-    --         -- assign the values to the inputs of the DUT (design under test)
+            tx_data <= test_case_array(ii).tx_data;
 
-    --         rx_data_B <= test_case_array(i).tx_data; 
-    --         -- value1 <= test_case_array(i).value1;
-    --         -- value2 <= test_case_array(i).value2;
-    --         -- value3 <= test_case_array(i).value3;
-    --         -- bet1_wins <= test_case_array(i).bet1_wins;
-    --         -- bet2_wins <= test_case_array(i).bet2_wins;
-    --         -- bet3_wins <= test_case_array(i).bet3_wins;              
+            -- wait until UART_TXD = '0'; -- detect that first bit is being sent
+            for i in test_case_array(ii).packet'low to test_case_array(ii).packet'high loop  
+                       
+                int <= i; -- for debugging
+                tx_data <= test_case_array(ii).tx_data;
+                wait for bit_period/2*12;
+            end loop;
 
-    --         -- wait for some time, to give the DUT circuit time to respond (1ns is arbitrary)                
+            report "Expected result= " &  
+                    integer'image(to_integer(unsigned(test_case_array(ii).rx_data))) &
+            "  Actual result= " &  
+                    integer'image(to_integer(unsigned(rx_data_B)));
+            assert (test_case_array(ii).rx_data = rx_data_B)
+                report "MISMATCH.  THERE IS A PROBLEM IN YOUR DESIGN THAT YOU NEED TO FIX"
+                severity warning;
 
-    --         -- wait for 1 ns;
-    --         wait for transmit_wait*768 ps;
-            
-    --         -- now print the results along with the expected results
-            
-    --         report "Expected result= " &  
-    --                     integer'image(to_integer(unsigned(test_case_array(i).rx_data))) &
-    --             "  Actual result= " &  
-    --                     integer'image(to_integer(unsigned(rx_data)));
+        end loop;
 
-    --         -- This assert statement causes a fatal error if there is a mismatch
-                                                                        
-    --         assert (test_case_array(i).rx_data = rx_data )
-    --             report "MISMATCH.  THERE IS A PROBLEM IN YOUR DESIGN THAT YOU NEED TO FIX"
-    --             severity warning;
-    --     end loop;
-                                            
-    --     report "================== ALL TESTS PASSED =============================";
+        report "================== ALL TESTS PASSED =============================";
                                                                                 
-    --     wait; --- we are done.  Wait for ever
-    -- end process;
+        wait; --- we are done.  Wait for ever
+    end process;
 
 end tb;
